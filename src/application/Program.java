2b7a2b210705e13b2a40e2d51b0afeeaa5df3d0e
sbm-landrade <lucas.andrade@sbmtechnology.com.br>
2025-01-21 21:29:05 -0300

    implement findByDepartment e findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -21,6 +23,19 @@ public class Program {
 		System.out.println("=== Test 1: seller findBy ====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		
+		System.out.println("=== Test 2: seller findByDepartment ====");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("=== Test 3: seller findAll ====");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }

0d63baf6283f938420769c207e03d020d3e9d3de
sbm-landrade <lucas.andrade@sbmtechnology.com.br>
2025-01-26 20:16:31 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,17 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() { //Para não expor a implementação
 		return new SellerDaoJDBC(DB.getConnection()); //Somente a interface
+	}
+
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 
 }
